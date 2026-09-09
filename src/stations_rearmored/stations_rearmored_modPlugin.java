0520cd9f44b6b609c0b8d67595e12853887b610e
Vexlia-Artemiss <vexlia.artist@gmail.com>
2026-09-09 19:08:46 +0300

    Update stations_rearmored_modPlugin.java

--- src/stations_rearmored/stations_rearmored_modPlugin.java
+++ src/stations_rearmored/stations_rearmored_modPlugin.java
@@ -2,77 +2,102 @@ package stations_rearmored;
 
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.ShipVariantAPI;
+import org.magiclib.hullmods.enhancedmodule.EnhancedModuleArmor;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class stations_rearmored_modPlugin extends BaseModPlugin {
 
-    @Override
-    public void onApplicationLoad() throws Exception {
-        List<String> variants = Global.getSettings().getAllVariantIds();
-        for (String var : variants){
+    // Base hull/module ids whose variants should receive the enhanced module armor hullmod.
+    private static final List<String> ARMOR_MODULE_BASE_IDS = Arrays.asList(
             //Lowtech and partially midline
-            AddArmorHullmods("module_armour1", var);
-            AddArmorHullmods("module_armour2", var);
+            "module_armour1",
+            "module_armour2",
 
             //Midline
-            AddArmorHullmods("module_armour3", var);
-            AddArmorHullmods("module_armour4", var);
-            AddArmorHullmods("module_armour5", var);
+            "module_armour3",
+            "module_armour4",
+            "module_armour5",
 
             //Hightech
-            AddArmorHullmods("module_hightech_armour", var);
+            "module_hightech_armour",
 
             //Remnant
-            AddArmorHullmods("remnant_armour1", var);
-            AddArmorHullmods("remnant_armour2", var);
-            AddArmorHullmods("remnant_armour3", var);
+            "remnant_armour1",
+            "remnant_armour2",
+            "remnant_armour3",
 
             //Shadow Yards
-            AddArmorHullmods("ms_blocker", var);
-            AddArmorHullmods("ms_module_armour", var);
+            "ms_blocker",
+            "ms_module_armour",
 
             //AOTD Citadels
             //Low Tech
-            AddArmorHullmods("armor_big", var);
-            AddArmorHullmods("armor_small", var);
+            "armor_big",
+            "armor_small",
             //High Tech
-            AddArmorHullmods("t4_ht_amodule", var);
+            "t4_ht_amodule",
 
             //Eusan Nation
-            AddArmorHullmods("eusan_nation_module_armour", var);
-            AddArmorHullmods("eusan_nation_module_armour_s", var);
+            "eusan_nation_module_armour",
+            "eusan_nation_module_armour_s",
 
             //XLU - Blue
-            AddArmorHullmods("xlu_station_armor", var);
-            AddArmorHullmods("xlu_station_bastion_armor", var);
-            AddArmorHullmods("xlu_station_blocker_armor_left", var);
-            AddArmorHullmods("xlu_station_blocker_armor_right", var);
-            AddArmorHullmods("xlu_station_hangar_armor_left", var);
-            AddArmorHullmods("xlu_station_hangar_armor_right", var);
-            AddArmorHullmods("xlu_station_missile_armor_left", var);
-            AddArmorHullmods("xlu_station_missile_armor_right", var);
+            "xlu_station_armor",
+            "xlu_station_bastion_armor",
+            "xlu_station_blocker_armor_left",
+            "xlu_station_blocker_armor_right",
+            "xlu_station_hangar_armor_left",
+            "xlu_station_hangar_armor_right",
+            "xlu_station_missile_armor_left",
+            "xlu_station_missile_armor_right",
 
             //HMI - Mess
-            AddArmorHullmods("mess_remnant_armour1", var);
-            AddArmorHullmods("mess_remnant_armour2", var);
-            AddArmorHullmods("mess_remnant_armour3", var);
+            "mess_remnant_armour1",
+            "mess_remnant_armour2",
+            "mess_remnant_armour3",
 
             //Interstellar Imperium
-            AddArmorHullmods("ii_module_armor_left", var);
-            AddArmorHullmods("ii_module_armor_right", var);
-            AddArmorHullmods("ii_module_armor_straight", var);
-            AddArmorHullmods("ii_module_armor_leftsmall", var);
-            AddArmorHullmods("ii_module_armor_rightsmall", var);
-            AddArmorHullmods("ii_module_armor_leftbig_heavy", var);
-            AddArmorHullmods("ii_module_armor_rightbig_heavy", var);
+            "ii_module_armor_left",
+            "ii_module_armor_right",
+            "ii_module_armor_straight",
+            "ii_module_armor_leftsmall",
+            "ii_module_armor_rightsmall",
+            "ii_module_armor_leftbig_heavy",
+            "ii_module_armor_rightbig_heavy"
+    );
+
+
+    @Override
+    public void onApplicationLoad() throws Exception {
+
+        if (Global.getSettings().getHullModSpec("ML_enhancedModuleArmor") == null) {
+            throw new Exception("[stations_rearmored] Missing required hullmod - ML_enhancedModuleArmor!\nThis likely means you didn't install latest MagicLib pre-release.\nYou can install latest MagicLib pre-release from https://github.com/MagicLibStarsector/MagicLib/releases");
+        }
+
+        List<String> variantIds = Global.getSettings().getAllVariantIds();
+
+        for (String variantId : variantIds) {
+            try {
+                applyArmorHullmodIfMatch(variantId);
+            } catch (Exception e) {
+                Global.getLogger(this.getClass()).warn("Failed to process variant '" + variantId + "'", e);
+            }
         }
     }
 
-    void AddArmorHullmods(String hull_id, String variant){
-        if(variant.startsWith(hull_id)){
-            Global.getSettings().getVariant(variant).addMod("ML_enhancedModuleArmor");
-            Global.getSettings().getVariant(variant).addPermaMod("ML_enhancedModuleArmor");
+    private void applyArmorHullmodIfMatch(String variantId) {
+        for (String baseId : ARMOR_MODULE_BASE_IDS) {
+            if (variantId.startsWith(baseId)) {
+                ShipVariantAPI variant = Global.getSettings().getVariant(variantId);
+                variant.addMod(EnhancedModuleArmor.HULL_MOD_ID);
+                variant.addPermaMod(EnhancedModuleArmor.HULL_MOD_ID);
+                return; // a variant should only match one base id
+            }
         }
     }
 }
+
+
